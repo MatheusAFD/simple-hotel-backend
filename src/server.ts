dbd8ae7adb15390570272711719b9d9d00d78719
matheusafd <matheus.daf@outlook.com>
2022-09-29 21:32:20 -0300

    dependencies fix

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
+
 const app = express();
+const PORT = 3333;
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
-app.listen(3333, () => console.log("Server is running on PORT 3333"));
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
